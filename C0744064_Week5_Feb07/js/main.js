e735354639a0d72f1c817e637f1e20cf70f7f1ba
visualstudio <visualstudio@230-PC10>
2019-02-07 11:43:15 -0500

    working with arrays

--- C0744064_Week5_Feb07/js/main.js
+++ C0744064_Week5_Feb07/js/main.js
@@ -1,1 +1,11 @@
-﻿// Your code here!
+﻿function myfunction(a, b) {
+    var a = 5;
+    var b = 6;
+    return a + b;
+}
+
+function arrays() {
+    var pens;
+    pens = ["red", "orange", "blue", "black"];
+    console.log(pens);
+}
